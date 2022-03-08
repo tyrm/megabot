3ec955ed24d214110bc75849ab892e8d26a059a6
Tyr Mactire <tyr@pettingzoo.co>
2022-03-08 13:30:31 -0800

    id test

---
+++ internal/id/id_test.go
@@ -0,0 +1,51 @@
+package id
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"testing"
+)
+
+func TestNewULID(t *testing.T) {
+	for i := 1; i <= 5; i++ {
+		i := i
+		validate := validator.New()
+
+		name := fmt.Sprintf("[%d] Starting", i)
+		t.Run(name, func(t *testing.T) {
+			id, err := NewULID()
+			if err != nil {
+				t.Errorf("[%d] got error generating id: %s", i, err.Error())
+				return
+			}
+
+			err = validate.Var(id, "required,ulid")
+			if err != nil {
+				t.Errorf("[%d] %s is not a valid ulid: %s", i, id, err.Error())
+				return
+			}
+		})
+	}
+}
+
+func TestNewRandomULID(t *testing.T) {
+	for i := 1; i <= 5; i++ {
+		i := i
+		validate := validator.New()
+
+		name := fmt.Sprintf("[%d] Starting", i)
+		t.Run(name, func(t *testing.T) {
+			id, err := NewRandomULID()
+			if err != nil {
+				t.Errorf("[%d] got error generating id: %s", i, err.Error())
+				return
+			}
+
+			err = validate.Var(id, "required,ulid")
+			if err != nil {
+				t.Errorf("[%d] %s is not a valid ulid: %s", i, id, err.Error())
+				return
+			}
+		})
+	}
+}
