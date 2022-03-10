e3bfca6452f3a7b3a44b1500613e65bcc0f36438
Tyr Mactire <tyr@pettingzoo.co>
2022-03-09 21:39:59 -0800

    translate login

--- internal/language/text.go
+++ internal/language/text.go
@@ -4,6 +4,23 @@ import (
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
+// TextEmail returns a translated phrase.
+func (l *Localizer) TextEmail() string {
+	lg := logger.WithField("func", "TextEmail")
+
+	text, err := l.localizer.Localize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "Email",
+			Description: "the common phrase for email",
+			Other:       "Email",
+		},
+	})
+	if err != nil {
+		lg.Warningf("missing translation: %s", err.Error())
+	}
+	return text
+}
+
 // TextHelloWorld returns a translated phrase.
 func (l *Localizer) TextHelloWorld() string {
 	lg := logger.WithField("func", "TextHelloWorld")
@@ -28,7 +45,7 @@ func (l *Localizer) TextLogin() string {
 	text, err := l.localizer.Localize(&i18n.LocalizeConfig{
 		DefaultMessage: &i18n.Message{
 			ID:          "Login",
-			Description: "the word 'Login'",
+			Description: "the common phrase for logging in",
 			Other:       "Login",
 		},
 	})
@@ -47,6 +64,40 @@ func (l *Localizer) TextLoginInvalid() string {
 			ID:          "LoginInvalid",
 			Description: "Explains to the user that the email and password combination are invalid.",
 			Other:       "email/password combo invalid",
+		},
+	})
+	if err != nil {
+		lg.Warningf("missing translation: %s", err.Error())
+	}
+	return text
+}
+
+// TextLoginShort returns a translated phrase.
+func (l *Localizer) TextLoginShort() string {
+	lg := logger.WithField("func", "TextLogin")
+
+	text, err := l.localizer.Localize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "LoginShort",
+			Description: "a single word representation of logging in",
+			Other:       "Login",
+		},
+	})
+	if err != nil {
+		lg.Warningf("missing translation: %s", err.Error())
+	}
+	return text
+}
+
+// TextPassword returns a translated phrase.
+func (l *Localizer) TextPassword() string {
+	lg := logger.WithField("func", "TextPassword")
+
+	text, err := l.localizer.Localize(&i18n.LocalizeConfig{
+		DefaultMessage: &i18n.Message{
+			ID:          "Password",
+			Description: "the common phrase for password",
+			Other:       "Password",
 		},
 	})
 	if err != nil {
