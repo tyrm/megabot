789359bc89133eb512a65b3f9c73f5d3a50b8151
Tyr Mactire <tyr@pettingzoo.co>
2022-03-10 11:28:29 -0800

    test

--- internal/language/text_h_test.go
+++ internal/language/text_h_test.go
@@ -38,3 +38,37 @@ func TestTextHelloWorld(t *testing.T) {
 		})
 	}
 }
+
+func TestTextHomeShort(t *testing.T) {
+	langMod, _ := New()
+
+	tables := []struct {
+		x string
+		n string
+	}{
+		{"en", "Home"},
+		{"es", "Inicio"},
+		{"in", "Home"},
+	}
+
+	for i, table := range tables {
+		i := i
+		table := table
+
+		name := fmt.Sprintf("[%d] Translating to %s", i, table.x)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			localizer, err := langMod.NewLocalizer(table.x)
+			if err != nil {
+				t.Errorf("[%d] can't get localizer for %s: %s", i, table.x, err.Error())
+				return
+			}
+
+			result := localizer.TextHomeShort()
+			if result != table.n {
+				t.Errorf("[%d] got invalid translation for %s, got: %v, want: %v,", i, table.x, result, table.n)
+			}
+		})
+	}
+}
