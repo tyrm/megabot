55a56a2c264e7967f3e008ac5aa3cc7d60175094
Tyr Mactire <tyr@pettingzoo.co>
2022-03-10 20:19:55 -0800

    embed less files

--- main.go
+++ main.go
@@ -4,5 +4,10 @@ import "embed"
 
 // Files contains static files required by the application
 //go:embed active.*.toml
-//go:embed web/static/*  web/template/*
+//go:embed web/static/css/*
+//go:embed web/static/img/*
+//go:embed web/static/js/*
+//go:embed web/static/vendor/fontawesome-free-6.0.0-web/css/all.min.css
+//go:embed web/static/vendor/fontawesome-free-6.0.0-web/webfonts/*
+//go:embed web/template/*
 var Files embed.FS
