cfc1b87b55554ab77645b1b36b3c477a3bc92062
Tyr Mactire <tyr@pettingzoo.co>
2022-03-12 18:47:53 -0800

    errors

--- internal/db/bun/client_test.go
+++ internal/db/bun/client_test.go
@@ -2,6 +2,7 @@ package bun
 
 import (
 	"context"
+	"fmt"
 	"github.com/spf13/viper"
 	"github.com/tyrm/megabot/internal/config"
 	"testing"
@@ -19,6 +20,30 @@ func TestSqliteConn(t *testing.T) {
 	}
 	if bun == nil {
 		t.Errorf("client is nil")
+		return
+	}
+}
+
+func TestSqliteConn_NoConfig(t *testing.T) {
+	viper.Reset()
+
+	_, err := sqliteConn(context.Background())
+	errText := fmt.Sprintf("'%s' was not set when attempting to start sqlite", config.Keys.DbAddress)
+	if err.Error() != errText {
+		t.Errorf("unexpected error initializing sqlite connection, got: '%s', want: '%s'", err.Error(), errText)
+		return
+	}
+}
+
+func TestSqliteConn_BadPath(t *testing.T) {
+	viper.Reset()
+
+	viper.Set(config.Keys.DbAddress, "invalidir/db.sqlite")
+
+	_, err := sqliteConn(context.Background())
+	errText := "sqlite ping: Unable to open the database file (SQLITE_CANTOPEN)"
+	if err.Error() != errText {
+		t.Errorf("unexpected error initializing sqlite connection, got: '%s', want: '%s'", err.Error(), errText)
 		return
 	}
 }
