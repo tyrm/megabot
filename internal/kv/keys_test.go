d0468319a83a199f4142ee27ce7e4fd80093fd68
Tyr Mactire <tyr@pettingzoo.co>
2022-03-27 16:03:54 -0700

    test

--- internal/kv/keys_test.go
+++ internal/kv/keys_test.go
@@ -5,13 +5,20 @@ import "testing"
 func TestKeyJwtAccess(t *testing.T) {
 	v := KeyJwtAccess("test123")
 	if v != "megabot:jwt:a:test123" {
-		t.Errorf("enexpected value for KeyDomains, got: '%s', want: 'sr:jwt:a:test123'.", v)
+		t.Errorf("enexpected value for KeyDomains, got: '%s', want: 'megabot:jwt:a:test123'.", v)
 	}
 }
 
 func TestKeyJwtRefresh(t *testing.T) {
 	v := KeyJwtRefresh("test123")
 	if v != "megabot:jwt:r:test123" {
-		t.Errorf("enexpected value for KeyDomains, got: '%s', want: 'sr:jwt:r:test123'.", v)
+		t.Errorf("enexpected value for KeyDomains, got: '%s', want: 'megabot:jwt:r:test123'.", v)
+	}
+}
+
+func TestKeySession(t *testing.T) {
+	v := KeySession()
+	if v != "megabot:session:" {
+		t.Errorf("enexpected value for KeyDomains, got: '%s', want: 'megabot:session:'.", v)
 	}
 }
